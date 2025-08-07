06610ace3e73d11c9e71a2e197741d80b125e0aa
kofiokrah <knyamokrah@gmail.com>
2025-08-07 19:44:09 +0000

    initial commit

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 const myform = document.querySelector('#myform');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
+const nameInput = document.querySelector('#name'); // Added missing selector
+const emailInput = document.querySelector('#email'); // Fixed selector
 const msg = document.querySelector('#msg');
-const userList = document.querySelector('#users');
+const userList = document.querySelector('#user');
 
-myform.addEventListener('submit', onSubmit);
+document.querySelector('form').addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
     e.preventDefault();
 
-   if (nameInput.value === '' || emailInput.value === '') {
+    if (nameInput.value === '' || emailInput.value === '') {
         msg.classList.add('error');
         msg.innerHTML = 'Please enter all fields';
-        setTimeout(() => msg.remove(), 3000);
+        setTimeout(() => msg.innerHTML = '', 3000);
     } else {
         const li = document.createElement('li');
-        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        li.appendChild(document.createTextNode(${nameInput.value} : ${emailInput.value}));
         userList.appendChild(li);
 
         // Clear fields
         nameInput.value = '';
-        emailInput.value = '';
-    }
+        emailInput.value = '';
+    }
+}
